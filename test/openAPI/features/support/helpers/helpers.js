bc4a53da79b2929dbde0eb208817834916626336
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2023-04-11 05:15:07 +0200

    TECH-527: Created Cucumber tests for /status/email endpoint

--- test/openAPI/features/support/helpers/helpers.js
+++ test/openAPI/features/support/helpers/helpers.js
@@ -1,4 +1,41 @@
 module.exports = {
   localhost: 'http://localhost:3333/',
   defaultResponseTime: 15000,
+  statusEmailEndpoint: 'status/email',
+  contentTypeHeader: {
+    key: 'content-type',
+    value: 'application/json; charset=utf-8',
+  },
+  allowHeader: {
+    key: 'allow',
+    value: 'POST',
+  },
+  acceptHeader: {
+    key: 'Accept',
+    value: 'application/json',
+  },
+  statusEmailResponseSchema: {
+    type: 'object',
+    properties: {
+      scheduled: { type: 'number' },
+      processing: { type: 'number' },
+      deferred: { type: 'number' },
+      bounced: { type: 'number' },
+      'not sent': { type: 'number' },
+      delivered: { type: 'number' },
+    },
+  },
+  doArraysHaveTheSameElements: (array1, array2) => {
+    if (array1.length === array2.length) {
+      return array1.every(element => {
+        if (array2.includes(element)) {
+          return true;
+        }
+
+        return false;
+      });
+    }
+
+    return false;
+  },
 };
