55a3f1f249cd9fea1ce64efe3fffd9d1e19a636b
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2023-04-11 08:27:37 +0200

    TECH-526: Added Cucumber tests for /callback/email endpoint

--- test/openAPI/features/support/helpers/helpers.js
+++ test/openAPI/features/support/helpers/helpers.js
@@ -1,4 +1,21 @@
 module.exports = {
   localhost: 'http://localhost:3333/',
   defaultResponseTime: 15000,
+  callbackEmailEndpoint: 'callback/email',
+  contentTypeHeader: {
+    key: 'content-type',
+    value: 'application/json; charset=utf-8',
+  },
+  allowHeaderPost: {
+    key: 'allow',
+    value: 'POST',
+  },
+  allowHeaderGet: {
+    key: 'allow',
+    value: 'GET',
+  },
+  acceptHeader: {
+    key: 'Accept',
+    value: 'application/json',
+  },
 };
